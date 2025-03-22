d41c0d680e502a8865c2e3ca189b38baaaf90af0
장철환 <wkdcjfghks1@gmail.com>
2025-03-22 12:23:52 +0900

    Update 스타트와 링크.py

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 
-
+# 부분집합을 재귀로 생성하며,
+# 부분집합을 방문한 적이 있는지,
+# 부분집합 개수를 충족했는지를 체크하며
+# 시너지의 최소값을 구한다.
 def dfs(line):
     global result
     if visited[tuple(line)]:
@@ -16,6 +19,7 @@ def dfs(line):
         dfs(line + [t])
 
 
+# 각 부분집합들의 시너지 합을 구하기 위한 함수
 def sum_list(li):
     test = 0
     for a in li:
@@ -27,8 +31,10 @@ def sum_list(li):
 
 N = int(input())
 stat = [list(map(int, input().split())) for _ in range(N)]
+# visited를 통해 한번 방문한 부분집합은 통과하게 만든다.
 visited = defaultdict(bool)
 result = float('inf')
+# 부분집합을 시작할 숫자
 for i in range(N):
     dfs([i])
 print(result)
